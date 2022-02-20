2410820aafae67609ef26b870164acb37285e4af
Kwansleep <sleepmanleung@gmail.com>
2022-02-19 22:24:37 -0800

    update compile.sh to make directory if not present

--- compileAndMake.sh
+++ compileAndMake.sh
@@ -1,3 +1,7 @@
+# executes cmake and make
+
 # make directory and compile at nfs-shared folder
 mkdir -p ~/cmpt433/public/project
 cmake -S . -B ~/cmpt433/public/project
+cd ~/cmpt433/public/project
+make
