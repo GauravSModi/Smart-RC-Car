27362aa8a6791d73b5f26cb6a408c44a1f05b4a7
Kwansleep <sleepmanleung@gmail.com>
2022-02-21 16:45:16 -0800

    declare joystick_test
    Enable toggling of tests building

---
+++ tests/joystick_test.cpp
@@ -0,0 +1,24 @@
+#include <joystick/joystick.h>
+
+
+// test if joystick module is include
+void testInclude(){
+  joystickDummy();
+}
+
+// test joystick init
+void testInit(){
+  printf("TEST: Initializing joystick.\n");
+ 
+  Joystick::initialize();
+  sleep(10);
+
+  printf("TEST: Shutting down joystick.\n");
+  Joystick::shutdown(); /**/
+}
+
+int main(int argc, char const *argv[]){
+  testInclude();
+
+  return 0;
+}
