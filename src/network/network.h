a41ee3b011c46c9e1476fbeb69a22c7ed25ba3f5
Kwansleep <sleepmanleung@gmail.com>
2022-04-04 15:35:47 -0700

    fix merge conflicts artifacts

--- src/network/network.h
+++ src/network/network.h
@@ -9,14 +9,14 @@
 #include <unistd.h>			// for close()
 #include <functional>
 #include <string>
-/*udp file: mainly focus on showing history sample data, and packet handling*/
 #include <thread>
 #include "rover/rover.h"
-
+/*udp file: mainly focus on showing history sample data, and packet handling*/
 
 #define MSG_MAX_LEN 1500
 
-void init_udp(std::function<void()> shutdownFunction);
+void init_udp(std::function<void()> shutdownFunction, rover* myRover);
 void clean_udp(void);
 
 void udp_reply(struct sockaddr_in sinRemote,int socketDescriptor, std::string message);
+
