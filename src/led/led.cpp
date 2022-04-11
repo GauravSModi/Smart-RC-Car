c9500bc2bba2b90d7e5b44f54caafa5c4a98aa71
Kwansleep <sleepmanleung@gmail.com>
2022-04-11 09:53:55 -0700

    update LED module to include flashing behaviour

--- src/led/led.cpp
+++ src/led/led.cpp
@@ -1,9 +1,63 @@
 #include "led.h"
 
-#include <cstdio>
-#include <stdlib.h>
-#include <unistd.h>
+#include <iostream>
+//#include <stdlib.h>
+//#include <unistd.h>
 #include <common/utils.h>
+#include <thread>
+
+#define FLASH_INTERVAL_MS 250
+
+std::thread* ledThread;
+static bool isRunning;
+static bool shouldFlash = false;
+
+
+static void ledMainThreadRun();
+
+void init_LEDModule(){
+    isRunning = true;
+    ledThread = new std::thread(ledMainThreadRun);
+}
+
+void cleanUp_LEDModule(){
+    isRunning = false;
+    ledThread->join();
+    delete ledThread;
+}
+
+void startFlashing(){
+    shouldFlash = true;
+}
+void stopFlashing(){
+    shouldFlash = false;
+}
+
+void ledMainThreadRun(){
+
+    while(isRunning){
+        if(!shouldFlash){
+            turnOnLED(ZERO_LED_DIR_PATH);
+            turnOnLED(FIRST_LED_DIR_PATH);
+            turnOnLED(SECOND_LED_DIR_PATH);
+            turnOnLED(THIRD_LED_DIR_PATH);
+        }
+        else if(shouldFlash){
+            turnOffLED(ZERO_LED_DIR_PATH);
+            turnOffLED(FIRST_LED_DIR_PATH);
+            turnOffLED(SECOND_LED_DIR_PATH);
+            turnOffLED(THIRD_LED_DIR_PATH);
+            msleep(FLASH_INTERVAL_MS);
+            turnOnLED(ZERO_LED_DIR_PATH);
+            turnOnLED(FIRST_LED_DIR_PATH);
+            turnOnLED(SECOND_LED_DIR_PATH);
+            turnOnLED(THIRD_LED_DIR_PATH);
+        }
+
+        msleep(FLASH_INTERVAL_MS);
+    }
+    
+}
 
 
 void ledDummy(){
@@ -20,7 +74,7 @@ void turnOffLED(std::string lEDPath){
 
 bool flashLED(std::string lEDPath, int forMiliseconds){
     truncateToFile(lEDPath + "brightness","1");
-    usleep(forMiliseconds * 1000);
+    msleep(forMiliseconds);
     truncateToFile(lEDPath + "brightness","0");
     return true;
 }
--- src/led/led.h
+++ src/led/led.h
@@ -1,7 +1,19 @@
 #pragma once
+#include <string>
 
-void turnOnLED(const char* lEDPath);
-void turnOffLED(const char* lEDPath);
-bool flashLED(const char* lEDPath, int forMiliseconds);
+#define ZERO_LED_DIR_PATH "/sys/class/leds/beaglebone:green:usr0/"
+#define FIRST_LED_DIR_PATH "/sys/class/leds/beaglebone:green:usr1/"
+#define SECOND_LED_DIR_PATH "/sys/class/leds/beaglebone:green:usr2/"
+#define THIRD_LED_DIR_PATH "/sys/class/leds/beaglebone:green:usr3/"
+
+void init_LEDModule();
+void cleanUp_LEDModule();
+
+void startFlashing();
+void stopFlashing();
+
+void turnOnLED(std::string lEDPath);
+void turnOffLED(std::string lEDPath);
+bool flashLED(std::string lEDPath, int forMiliseconds);
 void ledDummy();
 
